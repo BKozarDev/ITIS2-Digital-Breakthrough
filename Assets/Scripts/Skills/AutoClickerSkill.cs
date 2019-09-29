0c36ddbdbf26fad8555469ee965f31993d266243
Zung <zung.nguyen.98@gmail.com>
2019-09-29 09:33:47 +0300

    Revert "Revert "Merge branch 'Coding' of https://github.com/KozarBogdan11603/ITIS2-Digital-Breakthrough into Coding""
    This reverts commit 41c2a998f7e4f6ef3fc193c93cc8702e391f5a3c.

---
+++ Assets/Scripts/Skills/AutoClickerSkill.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AutoClicker", menuName = "Skills/Auto Clicker")]
+public class AutoClickerSkill : Skill
+{
+    public override void SkillActivation()
+    {
+        Debug.Log("Clicker! " + this.UpgradeLvl);
+    }
+}
---
+++ Assets/Scripts/Skills/ExtinguishSkill.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Extinguish", menuName = "Skills/Extinguish")]
+public class ExtinguishSkill : Skill
+{
+    public override void SkillActivation()
+    {
+        Debug.Log("DIE INSECT! " + this.UpgradeLvl);
+    }
+}
